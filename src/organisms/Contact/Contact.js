85e56d3db53acaf1ae690d4069cf50df0531cf08
Bartlomiej95 <panfilbartlomiej@gmail.com>
2020-12-06 18:46:41 +0100

    Add validation to contact form
    Validated with react-hook-form

--- src/organisms/Contact/Contact.js
+++ src/organisms/Contact/Contact.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useForm } from "react-hook-form";
 import { SubHeading } from '../../components/Heading/Heading';
 import { FormButton } from '../../components/Button/Button';
 
@@ -82,23 +83,74 @@ const Form = styled.form`
 
 `;
 
+const nameErrors = {
+    required: true,
+    minLength: 2,
+    maxLength: 15,
+}
+
+const titleErrors = {
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+} 
+
+const emailError = {
+    pattern: /^[a-z\d]+[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i
+}
+
+const nameErrorsFn = type => {
+    switch(type){
+        case'required':
+            return <span>Imię jest wymagane</span>
+        case 'minLength':
+            return <span>Imię musi zawierać co najmniej 2 znaki</span>
+        case 'maxLength':
+            return <span>Imię musi zawierać co najwyżej 15 znaków</span>
+        default:
+            return null;
+    }
+} 
+
+const titleErrorsFn = type => {
+    switch(type){
+        case'required':
+            return <span>Tytuł jest wymagany</span>
+        case 'minLength':
+            return <span>Tytuł musi zawierać co najmniej 2 znaki</span>
+        case 'maxLength':
+            return <span>Tytuł musi zawierać co najwyżej 50 znaków</span>
+        default:
+            return null;
+    }
+} 
+
+
 const Contact = () => {
+
+    const { register, handleSubmit, errors } = useForm();
+
+    const onSubmit = data => console.log(data);
+    console.log(errors);
     return(
         <Wrapper id="contact">
             <SubHeading>Kontakt</SubHeading>
             <FormWrapper>
-                <Form >
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <label> 
-                        <input type="text" placeholder="Imię" name="name"/>
+                        <input type="text" placeholder="Imię" name="name" ref={register(nameErrors)} />
+                        {nameErrorsFn(errors.name?.type)}
                     </label>
                     <label>
-                        <input type="text" placeholder="Email" name="email"/>
+                        <input type="text" placeholder="Email" name="email" ref={register(emailError)}/>
+                        {errors.email && <span>Podaj poprawny adres email</span>}
                     </label>
                     <label>
-                        <input type="text" placeholder="Temat" name="title"/>
+                        <input type="text" placeholder="Temat" name="title" ref={register(titleErrors)}/>
+                        {titleErrorsFn(errors.title?.type)}
                     </label>
                     <label>
-                        <textarea type="textarea" placeholder="Treść..." name="content"/>
+                        <textarea type="textarea" placeholder="Treść..." name="content"ref={register()} />
                     </label>
                     <FormButton>Wyślij</FormButton>
                 </Form>
